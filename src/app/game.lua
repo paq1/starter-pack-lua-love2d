297c2ec2014b3ed9cdd3c915bb572b0b5b757c6a
Pierre Paquin <p.paquin1016@laposte.net>
2023-12-17 09:10:58 +0100

    refactor: clean code

--- src/app/game.lua
+++ src/app/game.lua
@@ -1,20 +1,16 @@
 local game_app = {}
 
 local animation = require("src/app/animation/Animation")
-local PlayerService = require("src/app/player/playerService")
+local PlayerService = require("src/core/actor/player/playerService")
 local KeyboardService = require("src/app/keyboard/KeyboardService")
 
 function game_app.load()
-    loaded = false
-    whale = love.graphics.newImage("assets/sprites/actor/whale.png")
     keyboardService = KeyboardService:new()
 
     local snakeSpritesheet = love.graphics.newImage("assets/sprites/actor/Snake.png")
     playerAnimation = animation:new(snakeSpritesheet, 16, 16, 1)
 
     playerService = PlayerService:new(keyboardService, playerAnimation)
-    loaded = true
-    print("loaded\n")
 end
 
 function game_app.update(dt)
