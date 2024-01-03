d8f08502577b68a8763b71609e7e763cb32a19ff
Pierre Paquin <p.paquin1016@laposte.net>
2024-01-03 08:58:33 +0100

    chore: remise en place de la puissance des torches

--- src/core/map/ConfigMap.lua
+++ src/core/map/ConfigMap.lua
@@ -4,6 +4,6 @@ local ConfigMap = {}
 ConfigMap.offset = 32
 ConfigMap.tileSize = 32
 ConfigMap.size = {x = 300, y = 300}
-ConfigMap.torchBasePower = 1
+ConfigMap.torchBasePower = 32
 
 return ConfigMap
